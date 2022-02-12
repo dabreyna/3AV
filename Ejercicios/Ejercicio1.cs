e86daa0515e213b0b18dae798f748ddd8ecf67fa
Daniel Alberto Bruno Reyna <daniel@hexcode.com.mx>
2022-02-11 21:24:28 -0800

    Creacion del primer Ejercicio
    Se creo el arhivo Ejercicio1.cs que contiene el primer ejercicio del proyecto Base,
    se actualizo el archivo Program.cs, se agregaron el metodo main

---
+++ Ejercicios/Ejercicio1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Ejercicio1
+    {
+        public void saludo(string nombre){
+            System.Console.WriteLine("Bienvenido {0}",nombre);
+        }
+
+        public void despedir(){
+            System.Console.WriteLine("Nos vemos ya es viernes!!!!");
+        }
+    }
+}
--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ejercicios{
+
+    class Ejercicios{
+
+        public static void Main(string[] args){
+
+            Ejercicio1 saludar = new Ejercicio1();
+
+            saludar.saludo("Daniel Bruno");
+        
+        }
+    }
+}
