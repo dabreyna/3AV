e1f1e0359df99ff554e6d651ab468e2158f700a5
Daniel Alberto Bruno Reyna <daniel@hexcode.com.mx>
2022-02-15 19:32:20 -0800

    Actualizacion

---
+++ Ejercicios/Ejercicio2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Ejercicio2
+    {
+        public int sumar(int a, int b){
+            return a+b;
+        }
+
+        public int restar(int a, int b){
+            return a-b;
+        }
+
+        public int multiplicar(int num1,int num2){
+            return num1*num2;
+        }
+        public int dividir(int x,int y){
+            return x/y;
+        }
+
+    }
+}
--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,17 +1,49 @@
 ﻿using System;
-
 namespace Ejercicios{
 
     class Ejercicios{
 
         public static void Main(string[] args){
 
-            Ejercicio1 saludar = new Ejercicio1();
-            Console.WriteLine("Ingresa tu nombre: ");
-            string nombre = Console.ReadLine();
+            /*
 
-            saludar.saludo(nombre);
-        
+                    MENU
+
+                    1. EJERCICIO1
+                    2 
+                    ..
+
+                    55 EJERCICIO 55
+
+                    SELECCIONA UNA OPCION: 1
+
+
+            */
+
+
+            Ejercicio2 operaciones = new Ejercicio2();
+            int valor,num1,num2;
+
+            System.Console.WriteLine("Ingresa un numero: ");
+            num1=int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingresa otro numero: ");
+            num2=int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("\n\t SUMA");
+            valor=operaciones.sumar(num1,num2);
+            System.Console.WriteLine(valor);
+            
+            System.Console.WriteLine("\n\t RESTA");
+            valor = operaciones.restar(num1,num2);
+            System.Console.WriteLine(valor);
+
+            System.Console.WriteLine("\n\t MULTIPLICACION");
+            valor = operaciones.multiplicar(num1,num2);
+            System.Console.WriteLine(valor);
+
+            System.Console.WriteLine("\n\t DIVISION");
+            valor = operaciones.dividir(num1,num2);
+            System.Console.WriteLine(valor);
         }
     }
 }
